51212999423970f19ce35c837a62ce2a0164f1f2
Sachin <saror061@uottawa.ca>
2018-03-31 15:19:52 -0400

    Added Functionality to Create User Profile

--- app/src/main/java/com/example/sachin/learnwithme/CatActivity.java
+++ app/src/main/java/com/example/sachin/learnwithme/CatActivity.java
@@ -1,5 +1,6 @@
 package com.example.sachin.learnwithme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.sachin.learnwithme.data.CategoryData;
@@ -15,11 +19,12 @@ import com.example.sachin.learnwithme.data.DataService;
 import java.util.List;
 
 public class CatActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_cat);
+
 
 
         List<CategoryData> catData= DataService.getData();
@@ -29,6 +34,29 @@ public class CatActivity extends AppCompatActivity {
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this, catData );
         myrw.setLayoutManager(new GridLayoutManager(this, 3));
         myrw.setAdapter(myAdapter);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        Log.d("Test", "Cat Activity ....tool bar");
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+        if(id==R.id.action_settings){
+            Intent intent = new Intent(CatActivity.this, UserProfile.class);
+            startActivity(intent);
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
